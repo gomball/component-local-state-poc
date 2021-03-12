7a8d2fceef339084acac76771ebd18ab4f0092d8
Daniel Gómez <daniel.gomez@innocv.com>
2021-03-12 20:56:46 +0100

    add akita

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
+import { environment } from 'src/environments/environment';
 import { AppRootComponent } from './app-root/app-root.component';
 import { AppRoutingModule } from './app-routing.module';
 import { EmptyComponent } from './components/empty/empty.component';
@@ -8,7 +10,12 @@ import { SecondComponent } from './components/second/second.component';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  imports: [BrowserModule, AppRoutingModule, SharedModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    environment.production ? [] : AkitaNgDevtools.forRoot({ name: 'routing manager' }),
+    SharedModule,
+  ],
   declarations: [AppRootComponent, EmptyComponent, FirstComponent, SecondComponent],
   providers: [],
   bootstrap: [AppRootComponent],
