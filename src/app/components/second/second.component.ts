61ecf6f3bbd143e92a9ec46ad1793d7fa505dff8
Daniel Gómez <daniel.gomez@innocv.com>
2021-03-13 18:27:58 +0100

    destroy temp state on component destroy

--- src/app/components/second/second.component.ts
+++ src/app/components/second/second.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy } from '@angular/core';
 import { getTempState } from 'src/app/state/temp-state';
 import { GlobalQuery } from 'src/app/state/global/global.query';
 import { GlobalService } from 'src/app/state/global/global.service';
@@ -13,16 +13,20 @@ import { GlobalService } from 'src/app/state/global/global.service';
     </div>
     <div class="align-items-md-center d-flex my-1">
       <button type="button" class="btn btn-outline-secondary btn-sm mx-2" (click)="increment()">++</button>
-      local counter: {{ localStore.get$('counter') | async }}
+      local counter: {{ tempState.get$('counter') | async }}
     </div>`,
   styles: [],
 })
-export class SecondComponent {
-  readonly localStore = getTempState('second', { counter: 0 });
+export class SecondComponent implements OnDestroy {
+  readonly tempState = getTempState('second', { counter: 0 });
 
   constructor(public readonly globalService: GlobalService, public readonly globalQuery: GlobalQuery) {}
 
+  ngOnDestroy(): void {
+    this.tempState.destroy();
+  }
+
   increment(): void {
-    this.localStore.store.update((state) => ({ ...state, counter: state.counter + 1 }));
+    this.tempState.store.update((state) => ({ ...state, counter: state.counter + 1 }));
   }
 }
